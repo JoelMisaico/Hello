f391e2ea0bf94125416525fc99b1bf34741bd846
Joel Misaico <joelmisaicoperez@gmail.com>
2023-03-23 15:00:26 -0500

    commit initialii

---
+++ paquetes/main.go
@@ -0,0 +1,11 @@
+// Un paquete es simplemente una carpeta que contiene
+// una coleccion de archivos y nos provee una funcionalidad
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+
+	greet.English()
+}
